bbecd06143ca18756f23067d862e73f1f2cb276b
savage-sean91 <Savage.Sean91@gmail.com>
2022-02-14 19:29:31 -0500

    added chalk module
    added chalk!

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Load Express module
 const express = require("express");
+const  chalk  = require("chalk");
 
 // Create an Express object
 const app = express();
@@ -7,10 +8,11 @@ const port = 3000;
 
 // Setup the Get Route
 app.get("/", function (req, res) {
-  res.send("Hello World!");
+  res.send("Hello class!");
 });
 
 // list for request on port 3000
 app.listen(port, function () {
-  console.log(`Example app listening on port ${port}!`);
+  console.log(`Example app listening on port ` + chalk.green(port));
 });
+
--- index2.js
+++ index2.js
@@ -2,13 +2,14 @@
 const http = require("http");
 const hostname = "127.0.0.1";
 const port = 3000;
+const { chalk } = require("chalk");
 
 //Create HTTP server and listen on port 3000 for requests
 const server = http.createServer((req, res) => {
   //set the response Header with HTTP status and Content type
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World\n");
+  res.end("Hello class\n");
 });
 
 //listen for request on port 3000, and as a callback function have the port listened on logged
